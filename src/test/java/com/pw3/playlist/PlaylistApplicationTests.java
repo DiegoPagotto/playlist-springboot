1cb204ab027999c4e18822ec6456b984cb7823e5
DiegoPagotto <diegodeabreupagotto@gmail.com>
2022-11-15 17:15:21 -0300

    Initial Commit

---
+++ src/test/java/com/pw3/playlist/PlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pw3.playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
